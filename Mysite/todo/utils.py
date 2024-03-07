7af270bca13cac32a8cec47368fec26c02478bb7
anton <aiker03@mail.ru>
2024-03-08 00:29:45 +0300

    Создал utils, добавил в него список названий кнопок и ссылки на представления

---
+++ Mysite/todo/utils.py
@@ -0,0 +1,10 @@
+
+
+
+
+menu = [
+    {'title': "Менеджер задач", 'url_name': 'home'},
+    {'title': "Пользователи", 'url_name': 'users'},
+    {'title': "Метки", 'url_name': 'labels'},
+    {'title': "Задачи", 'url_name': 'tasks'}
+]
