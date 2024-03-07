9a8c62078b65284469c58485e14184b75ab00fed
anton <aiker03@mail.ru>
2024-03-08 00:21:45 +0300

    Подправил модель Task, добавив выбор состояния, к меткам отношения многие ко многим, описав состояния в модели

--- Mysite/todo/models.py
+++ Mysite/todo/models.py
@@ -4,17 +4,29 @@ from django.contrib.auth.models import User
 
 
 
-class State(models.Model):
+class Labels(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    state_name = models.CharField(max_length=255)
+    label_name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, verbose_name='URL', db_index=True)
 
 
-class Task(models.Model):
+class Tasks(models.Model):
+    NOT_STARTED = 'Not started'
+    IN_PROGRESS = 'In progress'
+    COMPLETE = 'Complete'
+
+    STATE_CHOICES = [
+        (NOT_STARTED, 'Not started'),
+        (IN_PROGRESS, 'In progress'),
+        (COMPLETE, 'Complete'),
+    ]
+
     slug = models.SlugField(max_length=255, unique=True, verbose_name='URL', db_index=True)
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
-    state = models.ForeignKey('State', on_delete=models.CASCADE)
+    state = models.CharField(max_length=20, choices=STATE_CHOICES, default=NOT_STARTED)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time_create = models.DateTimeField(auto_now_add=True)
+    labels = models.ManyToManyField(Labels)
+
 
