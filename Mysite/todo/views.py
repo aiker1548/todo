870834ae0ec4f22b04fb30d202d2f67bca5d9d01
anton <aiker03@mail.ru>
2024-03-08 15:18:30 +0300

    Реализовал tasks, labels, users ListViews

--- Mysite/todo/views.py
+++ Mysite/todo/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Task, State
+from django.urls import reverse_lazy
+from .models import Tasks, Labels
 from django.views import generic
 from .forms import RegistrationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from .utils import *
 
 def home_view(request):
     return render(request, 'todo/homePage.html')
+
 
 class RegisterView(generic.FormView):
     form_class = RegistrationForm
@@ -32,8 +35,90 @@ class LoginView(generic.FormView):
 
     def get_success_url(self):
         return redirect(self.success_url)
+
     
 
 def userLogout(request):
     logout(request)
     return redirect('home')
+
+
+class UsersListView(generic.ListView):
+    model = User
+    context_object_name = 'users'
+    allow_empty = True
+    template_name = 'todo/usersList.html'
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        user_menu = menu.copy()
+        context = super().get_context_data(**kwargs)
+        context['menu'] = user_menu
+        return context
+
+class LabelsListView(generic.ListView):
+    model = Labels
+    context_object_name = 'labels'
+    allow_empty = True
+    template_name = 'todo/labelsList.html'
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        user_menu = menu.copy()
+        context = super().get_context_data(**kwargs)
+        context['menu'] = user_menu
+        return context
+    
+
+class TasksListView(generic.ListView):
+    model = Tasks
+    context_object_name = 'tasks'
+    allow_empty = True
+    template_name = 'todo/tasksList.html'
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        user_menu = menu.copy()
+        context = super().get_context_data(**kwargs)
+        context['menu'] = user_menu
+        return context
+    
+
+# class TaskUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
+#     model = Tasks
+#     form_class = TaskUpdateForm
+#     template_name = 'todo/TaskUpdate.html'
+#     success_url = reverse_lazy('home')
+#     login_url = reverse_lazy('login')
+#     slug_url_kwarg = 'task_slug'
+
+#     def dispatch(self, request, *args, **kwargs):
+#         task = self.get_object()
+#         if task.user_id != self.request.user.pk:
+#             return redirect('home')
+#         return super().dispatch(request, *args, **kwargs)
+
+#     def form_valid(self, form):
+#         task = form.save()
+#         task.user = self.request.user
+#         task.save()
+#         return redirect('task', task_slug=task.slug)
+
+
+
+# class LabelUpdateView(LoginRequiredMixin, DataMixin, UpdateView):
+#     model = Labels
+#     form_class = LabelUpdateForm
+#     template_name = 'todo/labelUpdate.html'
+#     success_url = reverse_lazy('home')
+#     login_url = reverse_lazy('login')
+#     slug_url_kwarg = 'label_slug'
+
+#     def dispatch(self, request, *args, **kwargs):
+#         post = self.get_object()
+#         if post.user_id != self.request.user.pk:
+#             return redirect('home')
+#         return super().dispatch(request, *args, **kwargs)
+
+#     def form_valid(self, form):
+#         label = form.save()
+#         label.user = self.request.user
+#         label.save()
+#         return redirect('label', label_slug=label.slug)
